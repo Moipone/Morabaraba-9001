3756884f52320226c160263992faff0a8dbdf630
MoiponeT <moipone.tsaoane@gmail.com>
2018-04-14 01:25:10 +0200

    Create more initial Interfaces

---
+++ Morabaraba/IReferee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Morabaraba
+{
+    public interface IReferee
+    {
+        IPlayer Winner();
+        bool IsDraw();
+        void Play();
+
+    }
+}
