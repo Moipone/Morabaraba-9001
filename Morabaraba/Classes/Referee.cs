5003db8383bee308a134f48832ad1753a9cd01e4
YoungMiGo123 <martinkleinbooi@gmail.com>
2018-04-21 22:21:41 +0200

    Create checks in referee class, to test for flying, moving and placing

--- Morabaraba/Classes/Referee.cs
+++ Morabaraba/Classes/Referee.cs
@@ -67,7 +67,7 @@ namespace Morabaraba
             {
 
                 int cowPieces = cowBox.playerPiecesPositions(player).Count;
-                if (cowPieces == 3)
+                if (cowPieces > 3 && player.cowLives == 0)
                 {
                     Tile tTo = board.getTile(to);
                     Tile tFrom = board.getTile(from);
@@ -88,18 +88,41 @@ namespace Morabaraba
             return false;
         }
 
-        public void Play(string pos, IPlayer player)
+        public void playPlace(string pos, IPlayer player)
         {
             throw new NotImplementedException();
         }
+        public void playMove(string to, string from, IPlayer player)
+        {
+            if (isValidMove(to, from, player))
+            {
+                Tile tileTo = new Tile(to, new Piece(player.symbol, to));
+                Tile tileFrom = new Tile(from, new Piece(Symbol.BL, from));
 
+                board.updateTile(tileTo);
+                board.updateTile(tileFrom);
+            }
+            else Console.WriteLine("Invalid move, please make a valid move");
+        }
+        public void playFly(string to, string from, IPlayer player)
+        {
+            if (isValidFly(to, from, player))
+            {
+                Tile tileTo = new Tile(to, new Piece(player.symbol, to));
+                Tile tileFrom = new Tile(from, new Piece(Symbol.BL, from));
+
+                board.updateTile(tileTo);
+                board.updateTile(tileFrom);
+            }
+            else Console.WriteLine("Invalid move, please make a valid move");
+        }
         public IPlayer Winner(IPlayer p1, IPlayer p2)
         {
             int bP = cowBox.playerPiecesPositions(p1).Count;
             int wP = cowBox.playerPiecesPositions(p2).Count;
 
-            if (wP < 3) return p2;
             if (bP < 3) return p1;
+            if (wP < 3) return p2;
 
             return null;
         }
